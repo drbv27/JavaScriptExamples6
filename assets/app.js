b117f6a826ac426e169cab7ae2c9144b94c36047
Diego Bonilla <drbv27@gmail.com>
2021-05-12 06:36:40 -0500

    First Commit

---
+++ assets/app.js
@@ -0,0 +1,31 @@
+/*Ejercicios Basicos JS Nivel 6
+    18.Programa una función que dada una cadena de texto cuente el número de vocales y consonantes.Ejemplo: miFuncion("Hola Mundo") devuelva Vocales: 4, Consonantes: 5.
+    19.Programa una función que valide que un texto sea un nombre válido.Ejemplo miFuncion("Diego Bonilla") devolverá verdadero.
+    20.Programa una función que valide que un texto sea un email válido.Ejemplo: miFuncion("drbv27@gmail.com") devolverá verdadero.
+    */
+
+//Soluciones:
+
+//--------------------------------------------------------------------------------------------
+
+//E18S1.Solucion Simple:
+console.log("-----------------------------------------------------------------------------------")
+console.log("Ejercicio 18:")
+
+
+
+
+//--------------------------------------------------------------------------------------------
+
+//E19S1.Solución Simple
+console.log("-----------------------------------------------------------------------------------")
+console.log("Ejercicio 19:")
+
+
+
+//--------------------------------------------------------------------------------------------
+
+//E20S1.Solución Simple
+console.log("-----------------------------------------------------------------------------------")
+console.log("Ejercicio 20:")
+
